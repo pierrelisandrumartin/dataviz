817955367bb611cc314e021ae4fee646d387faa8
pierrelisandrumartin <pierrelisandrumartin@gmail.com>
2025-11-28 10:20:02 +0100

    new_coponents_new_oragnisation

--- src/components/components_graphs/Borough.tsx
+++ src/components/components_graphs/Borough.tsx
@@ -1,5 +1,73 @@
+import { useState } from "react";
+
+interface keys {
+  total_count: number;
+  results : Array<any>;
+}
+
 export default function Borough() {
-    return (
-        <h1>dgkjfjgj</h1>
-    )
+
+const paris: keys= {
+  total_count: 14760,
+  results: [
+    {
+      id_lieu: "2016-1939",
+      annee_tournage: "2016",
+      type_tournage: "S\u00e9rie TV",
+      nom_tournage: "LES HOMMES DE L'OMBRE",
+      nom_realisateur: "FRED GARSON",
+      nom_producteur: "MACONDO",
+      adresse_lieu: "AVENUE\u00a0 DU PRESIDENT WILSON",
+      ardt_lieu: "75016",
+      date_debut: "2016-02-13",
+      date_fin: "2016-02-13",
+      coord_x: 2.29457,
+      coord_y: 48.864614,
+      geo_shape: {
+        type: "Feature",
+        geometry: {
+          coordinates: [2.294570000235056, 48.864614000274074],
+          type: "Point",
+        },
+        properties: {},
+      },
+      geo_point_2d: { lon: 2.294570000235056, lat: 48.864614000274074 },
+    },
+    {
+      id_lieu: "2016-1495",
+      annee_tournage: "2016",
+      type_tournage: "Long m\u00e9trage",
+      nom_tournage: "MAMAN A TORT",
+      nom_realisateur: "MARC FITOUSSI",
+      nom_producteur: "AVENUE B",
+      adresse_lieu: "PLACE\u00a0 D'ITALIE",
+      ardt_lieu: "75013",
+      date_debut: "2016-01-06",
+      date_fin: "2016-01-06",
+      coord_x: 2.355564,
+      coord_y: 48.832294,
+      geo_shape: {
+        type: "Feature",
+        geometry: {
+          coordinates: [2.355563999715622, 48.83229399959034],
+          type: "Point",
+        },
+        properties: {},
+      },
+      geo_point_2d: { lon: 2.355563999715622, lat: 48.83229399959034 },
+    },
+  ],
+};
+    const [data, setData ] = useState(paris)
+
+  return (
+    <>
+      <h1> 
+        {data.results.map((item) => (
+        item.annee_tournage
+      ))} 
+      </h1>
+      <h1></h1>
+    </>
+  );
 }
---
+++ src/components/presentation/Presentation.tsx
@@ -0,0 +1,17 @@
+
+export default function Presentation(){
+    return (
+        <>
+        <h2>Présentation</h2>
+        <div className="presentation_container">
+            <div className="pierre_container">
+                <img src="" alt="" className="pierre_img" />
+            </div>
+            <div className="work_description_about_us"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia perspiciatis nostrum saepe. Ipsum nostrum quasi commodi alias non. Nobis ut, assumenda laborum voluptas hic, commodi aliquid eaque, rerum dolor animi quae aut accusamus cum sequi sit culpa? Dolor pariatur, expedita consequatur, adipisci iusto veritatis soluta eius totam sit qui dolore!</div>
+            <div className="franck_container">
+                <img src="" alt="" className="franck_img" />
+            </div>
+        </div>
+        </>
+    )
+}
